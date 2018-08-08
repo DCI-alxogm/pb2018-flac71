2c35cd05f71cf5ce526c58019091da1690997225
Fernanda Lizeth Andrade Cantu <andradecf2018@licifug.ugto.mx>
2018-08-08 09:37:32 -0500

    Cree un programa

---
+++ semana1/programa_hola.c
@@ -0,0 +1,11 @@
+/*Esto es un comentario. Me sirve para escribir información relevante del programa que estoy haciendo. Por ejemplo:
+Este programa imprime la típica frase "hola mundo a la pantalla"
+Creado el 06/08/2018 por Fernanda Andrade*/
+
+#include <stdio.h>
+int main()
+{	
+	printf("Hola Fernanda\n");
+	printf("Este es el primer programa del curso\n");
+}
+/*Termine de escribir el programa*/
