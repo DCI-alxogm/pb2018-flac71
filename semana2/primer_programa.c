bacdcb49158ff6bf4be87c913415838067f9c649
Fernanda Lizeth Andrade Cantu <andradecf2018@licifug.ugto.mx>
2018-08-15 09:58:38 -0500

    Actualizado el 15/08/2018

---
+++ semana2/primer_programa.c
@@ -0,0 +1,17 @@
+/*Creado el 15 de agosto de 2018 por Fernanda Andrade*/
+
+#include<stdio.h>
+
+int main()
+{
+	int edad;edad2
+
+	printf("Hola Fernanda \n");
+	printf("Este es el primer programa del curso");
+	printf("introduce tu edad: \n");
+	scanf("%i",edad);
+	printf("\nTu edad es: %i \n",edad);
+	edad2=edad+10;
+       printf("\nTu edad en el 2018 sera: %i \n",edad2);
+       return 0;
+}
