d289885acf5b9212102f8209c0e5aaa1e8942784
Fernanda Lizeth Andrade Cantu <andradecf2018@licifug.ugto.mx>
2018-09-03 09:58:35 -0500

    Añadi ejemplo de for

---
+++ semana5/ej_1for.c
@@ -0,0 +1,20 @@
+/*Este es un ejemplo de for, creado por Fernanda Andrade el 03 de septiembre del 2018*/
+//Añade una instrucción while para que el usuario pueda pedir ejecutar el programa nuevamente. Y haz que el usuario pueda definir las Temperatuas inicial (Temp_C), final (final) y numero de pasos (n).
+#include<stdio.h>
+int main (){
+	float TK, TC, final, delta;
+	int n,i;
+	
+	
+	printf("\n Añade la Temperatura inicial, Temperatura final y el número de veces que quieres que se realice la operación (n)\n");
+	scanf("%f", TC);
+	scanf("%f", final);
+	scanf("%i", n);
+	printf("\n Ahora realicemos las operaciones\n");
+	delta=(final-TC)/n;
+	printf("%f", delta);
+	for(i=0;i<n;i++){
+		TK=TC+273.15;
+		printf("%f %f\n", TK);
+		
+	
